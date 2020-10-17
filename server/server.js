24a7b4d7a67ee118e67ed8c35effe74fa718cb79
Darrik Moberg <30670444+mdarrik@users.noreply.github.com>
2020-10-17 15:42:35 -0700

    Comments some thoughts on tracing/events
    Added in some thoughts on where I might add entire traces or events. Because the io connections could potentially run _hours_, I'm not sure if it's better to collect all of the socket events throughout the trace or start new child traces for each one that get sent independently.

--- server/server.js
+++ server/server.js
@@ -13,7 +13,8 @@ const makeHandlers = require('./handlers')
 
 const clientManager = ClientManager()
 const chatroomManager = ChatroomManager()
-
+// Event: Connection Created, this is a long-running event that lives until the user disconnects. 
+//So might want multiple traces? One for this over-arching connection and some for each "event".  
 io.on('connection', function (client) {
   const {
     handleRegister,
@@ -23,28 +24,28 @@ io.on('connection', function (client) {
     handleGetChatrooms,
     handleGetAvailableUsers,
     handleDisconnect
-  } = makeHandlers(client, clientManager, chatroomManager)
+  } = makeHandlers(client, clientManager, chatroomManager) //make handlers gets called here
 
   console.log('client connected...', client.id)
   clientManager.addClient(client)
-
+  // register event fired - new trace/event here? 
   client.on('register', handleRegister)
-
+  // user joins channel - new trace/event here? 
   client.on('join', handleJoin)
-
+  // user leaves channel - new trace/event here?
   client.on('leave', handleLeave)
-
+  // user sends a message - new trace/event here? 
   client.on('message', handleMessage)
-
+  // user requests chatrooms - new trace/event here? 
   client.on('chatrooms', handleGetChatrooms)
-
+  // user requests users - new trace/event here? 
   client.on('availableUsers', handleGetAvailableUsers)
-
+  // user disconnects. End the io.onConnection trace at the end of this event? 
   client.on('disconnect', function () {
     console.log('client disconnect...', client.id)
     handleDisconnect()
   })
-
+   // error happens for the user, _should_ be attached to one of the other traces, but 
   client.on('error', function (err) {
     console.log('received error from client:', client.id)
     console.log(err)
